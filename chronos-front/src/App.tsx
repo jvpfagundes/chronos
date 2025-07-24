3184dd595c7028aa64b519fa2c23154bc4a99c66
joao-fagundes <joao.fagundes@speck.finance>
2025-07-24 20:10:32 -0300

    vite

--- chronos-front/src/App.tsx
+++ chronos-front/src/App.tsx
@@ -21,7 +21,7 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <ThemeProvider defaultTheme="system" storageKey="chronos-theme">
+    <ThemeProvider defaultTheme="light" storageKey="chronos-theme">
       <AuthProvider>
         <TooltipProvider>
           <Toaster />
